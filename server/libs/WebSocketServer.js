962c4fa2c97e15adecc9ba77fdde33c8115cf1e2
jim zike huang <iotpump.developer@gmail.com>
2017-04-06 16:51:44 -0700

    work in progress, much modular web socket server

---
+++ server/libs/WebSocketServer.js
@@ -0,0 +1,80 @@
+/**
+ * This is a general websocket server
+ */
+const ws = require('ws');
+const InternalDataChannel = require('./InternalDataChannel');
+const WebSocketServerChannel = require('./WebSocketServerChannel');
+const Config = require('./Config');
+
+module.exports=
+class WebSocketServer
+{
+  /**
+   * create a WebSocketServer
+   */
+  constructor(serverName)
+  {
+    var config = Config.get();
+    this.host = config.servers[serverName].host;
+    this.port = config.servers[serverName].port;
+    this.secretKey = config.servers[serverName].secretKey;
+
+    //we will first have the internal data channel (idc)
+    this.idc = new InternalDataChannel(serverName);
+    //all other channels here
+    this.channels = {};
+
+    //all clients here
+    this.clients = {};
+  }
+
+  addChannel(channelName)
+  {
+    var channel = new WebSocketServerChannel(this.serverName, channelName);
+    this.channels[channelName] = channel;
+  }
+
+  /**
+   * start the server
+   */
+  start()
+  {
+    this._serverInstance = new ws.Server({
+      perMessageDeflate: false,
+      host: this.host,
+      port: this.port
+    });
+    
+    this._serverInstance.on('connection', (ws) => {
+      if (typeof this.onConnection === "function") {
+        this.onConnection(ws); 
+      } else { //the custom onConnection method is not defined, now we do our channel message routing 
+        this._serverInstance.clients.forEach((client) => {
+          var clientChannelName = client.upgradeReq.url.slice(1);
+          if (clientChannelName == this.idc.getName() && client.readyState == ws.OPEN) {
+            this.handleIDCClient(client);
+          } else if (clientChannelName in this.channels && client.readyState == ws.OPEN) {
+            this.handleChannelClient(client);
+          } else { //all other clients are considered invalid, we just close them
+            client.close(); 
+          }
+        });
+      }
+    });
+  }
+
+  /**
+   * @TODO: stop the server
+   */
+  stop()
+  {
+    //this._serverInstance.close();
+  }
+}
+
+//test
+Config.init('../config.json');
+var server = new module.exports('s0');
+server.addChannel('abc');
+server.addChannel('efg');
+server.start();
