98b547318ee0a479bc889aad8d4b3364bc125987
jim zike huang <iotpump.developer@gmail.com>
2017-04-07 14:26:46 -0700

    store news in memory

--- server/libs/BroadCastingServer.js
+++ server/libs/BroadCastingServer.js
@@ -10,15 +10,27 @@ class BroadCastingServer extends WebSocketServer
 
   start()
   {
+    //store news from all sources in memory 
+    this.news = {};
+
     this.addChannel('latestnews');
+
+    var thisServer = this;
 
     var webSocket = this.connectToIDCOnServer('s0');
     webSocket.on('message', (message) => {
-      this.getChannel('latestnews').broadcast(message);
+      var messageObj = JSON.parse(message);
+      //instead of broadcasting the news to all clients, we just store them in memory
+      //this.getChannel('latestnews').broadcast(message);
+      thisServer.news[messageObj.source] = messageObj.articles;
     });
 
-    this.getChannel('latestnews').onMessage = (message) => {
-      console.log("received message " + message);
+    this.getChannel('latestnews').onMessage = (message, client) => {
+     if (client.readyState == client.OPEN) {
+      if (thisServer.news[message] != undefined) {
+          client.send(JSON.stringify(thisServer.news[message])); 
+        }
+      }
     }
 
     super.start(); //start the web server
