f5efb87273315c9fc2e430fa67fb6260c1db3241
jim zike huang <iotpump.developer@gmail.com>
2017-04-09 22:55:10 -0700

    add short id

--- server/libs/Path.js
+++ server/libs/Path.js
@@ -15,12 +15,18 @@ class Path
     this.channels = {};
 
     //add the default channel 
-    this.defaultChannelName = 'default_channel_' + Date.now();
+    this.defaultChannelName = this.path + '_default_channel';
     this.channels[this.defaultChannelName] = new Channel(this.defaultChannelName); 
+
+    //the client id couter for this path 
+    this.clientIdCounter = 0;
   }
 
   addConnectedClient(client)
   {
+    this.clientIdCounter ++;
+    client.id = this.clientIdCounter;
+    client.path = this.path;
     //add the client to the default channel 
     this.getDefaultChannel().addClient(client);
   }
