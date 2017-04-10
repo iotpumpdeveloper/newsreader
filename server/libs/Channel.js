10f0a2f9fe13e2739647b1b7ad95b94b8ae5f9fd
jim zike huang <iotpump.developer@gmail.com>
2017-04-09 23:17:11 -0700

    client id unique works

--- server/libs/Channel.js
+++ server/libs/Channel.js
@@ -7,29 +7,33 @@ class Channel
   constructor(name)
   {
     this.name = name;
-    this.clients = [];
+    this.clients = {};
   }
 
   addClient(client)
   {
     client.channel = this.name; //very important
-    this.clients.push(client);
+    this.clients[client.id]  = client;
     //hook up the client's message event 
     client.on('message', (message) => {
       this.onMessage(message, client);
     }); 
+    client.on('close', () => {
+      if (this.clients[client.id] != undefined) {
+        delete this.clients[client.id];
+      }
+    });
   }
 
   broadcast(message)
   {
-    for (var i = 0; i < this.clients.length; i ++) {
-      var client = this.clients.pop();
+    for (var clientId in this.clients) {
+      var client = this.clients[clientId];
       if (
         client.readyState == client.OPEN  //this client is still open
         && client.channel != undefined 
         && client.channel == this.name //this client is still under this channel
       ) { 
-        this.clients.unshift(client); //add this client back to this channel 
         //now the message can either be a string or a function that return custom message!
         if (typeof message == 'string') {
           client.send(message);
@@ -39,8 +43,9 @@ class Channel
             client.send(message);
           } 
         }
-      } 
-      //otherwise, the client will just be pop out and does not belong to this channel 
+      } else { //this client no longer belong to this channel, delete it
+        delete this.clients[clientId];
+      }
     }
 
   }
